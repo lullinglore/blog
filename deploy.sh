07751162d897796ca1d380dbcf95de5cae887673
Lulullia <47358820+Lulullia@users.noreply.github.com>
2024-03-06 18:52:31 +0100

    Bash test

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,18 @@
-echo "Cleaning and building "public" directory"
+echo 'Cleaning and building "public" directory'
 hugo --cleanDestinationDir
+
+echo -n 'Enter commit name:'
+read cname
+echo
+
+echo 'Pushing new changes to "public"'
+cd public
+git add .
+git commit -m "$cname"
+git push
+
+echo 'Pushing new changes to "blog"'
+cd ..
+git add .
+git commit -m "$cname"
+git push
